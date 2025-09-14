87be2aa785e148cf3323d2496c6eb733dd935803
Gabriel Augusto <gabrielsebe6@gmail.com>
2025-09-14 15:11:10 -0300

    Criando Containers dos Items

--- lista-de-compras/src/app/index.tsx
+++ lista-de-compras/src/app/index.tsx
@@ -1,30 +1,94 @@
 import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from "react-native";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Compras da Semana</Text>
 
-      <TextInput 
-      style={styles.input}
-      placeholder="Adicione um novo item" 
-      placeholderTextColor="9CA3AF" 
+      <TextInput
+        style={styles.input}
+        placeholder="Adicione um novo item"
+        placeholderTextColor="9CA3AF"
       />
 
       <TouchableOpacity style={styles.button}>
-        <text style={styles.textButton}>Adicionar Item</text>
+        <Text style={styles.textButton}>Adicionar Item</Text>
       </TouchableOpacity>
+
+        <View style={styles.cardConteiner}>
+          <View style={styles.card}>
+          <View style={styles.infocard}>
+            <MaterialCommunityIcons 
+            name="checkbox-blank-outline" 
+            size={20} 
+            color="#bcbfc5" 
+            />
+            <Text style={styles.textItem}> item 1</Text>
+          </View>
+          <MaterialIcons name="delete" size={24} color="#bcbfc5" />
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.infocard}>
+            <MaterialCommunityIcons 
+            name="checkbox-blank-outline" 
+            size={20} 
+            color="#bcbfc5" 
+            />
+            <Text style={styles.textItem}> item 1</Text>
+          </View>
+          <MaterialIcons name="delete" size={24} color="#bcbfc5" />
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.infocard}>
+            <MaterialCommunityIcons 
+            name="checkbox-blank-outline" 
+            size={20} 
+            color="#bcbfc5" 
+            />
+            <Text style={styles.textItem}> item 1</Text>
+          </View>
+          <MaterialIcons name="delete" size={24} color="#bcbfc5" />
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.infocard}>
+            <MaterialCommunityIcons 
+            name="checkbox-blank-outline" 
+            size={20} 
+            color="#bcbfc5" 
+            />
+            <Text style={styles.textItem}> item 1</Text>
+          </View>
+          <MaterialIcons name="delete" size={24} color="#bcbfc5" />
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.infocard}>
+            <MaterialCommunityIcons 
+            name="checkbox-blank-outline" 
+            size={20} 
+            color="#bcbfc5" 
+            />
+            <Text style={styles.textItem}> item 1</Text>
+          </View>
+          <MaterialIcons name="delete" size={24} color="#bcbfc5" />
+        </View>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: "#F4F5FB",
-      paddingHorizontal: 30,
-      paddingTop: 60,
-      alignItems: "center",
+    flex: 1,
+    backgroundColor: "#F4F5FB",
+    paddingHorizontal: 30,
+    paddingTop: 60,
+    alignItems: "center",
   },
 
   title: {
@@ -49,13 +113,43 @@ const styles = StyleSheet.create({
     height: 44,
     backgroundColor: "#CA3884",
     borderRadius: 12,
-    alignItems:"center",
+    alignItems: "center",
     justifyContent: "center",
+    marginBottom: 20,
   },
 
   textButton: {
     fontWeight: "bold",
     color: "#FFFFFF",
     fontSize: 16,
+  },
+
+  card: {
+    width: "100%",
+    height: 57,
+    backgroundColor: "#FFFFFF",
+    elevation: 15,
+    borderRadius: 12,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+
+  infocard: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+
+  textItem: {
+    color: "#374151",
+    fontSize: 16,
+  },
+
+  cardConteiner: {
+    width: "100%",
+    gap: 15,
   }
 });
